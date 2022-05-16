40b450c318c1d68564cbcc023d42e7fe82e1ed7a
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:19:46 -0400

    Agregando proyecto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+            double grados, radianes;
+            const double pi = 3.1416;
+            
+            Console.Write("Introduzca la cantidad de grados a convertir: ");
+            grados = double.Parse(Console.ReadLine());
+
+            radianes = (grados * pi) / 180;
+
+            Console.WriteLine("El resultado en radianes es: {0}", radianes);
